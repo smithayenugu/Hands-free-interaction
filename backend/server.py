781de8ddb202900bcf7f1dba0c5b9d6e2a9df85c
smithayenugu <156659498+smithayenugu@users.noreply.github.com>
2025-03-26 16:21:28 +0000

    Fixed CORS and API URLs

--- backend/server.py
+++ backend/server.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
 # Get absolute path to backend directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
